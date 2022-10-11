20a8c5428365c9cfabcafe4b143be413048a6549
leafcathead <leafcathead@gmail.com>
2022-10-11 18:03:36 +0200

    Java Application
    Barebones Java application

---
+++ AudienceResponseSystem/src/main/java/com.ars.alpha/AudienceResponseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ars.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudienceResponseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudienceResponseSystemApplication.class, args);
+	}
+
+}
