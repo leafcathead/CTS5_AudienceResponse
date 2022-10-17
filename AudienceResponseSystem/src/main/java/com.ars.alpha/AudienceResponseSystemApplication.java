070e84229563a06ead7ccadbabe60e80d20510f7
leafcathead <leafcathead@gmail.com>
2022-10-17 22:12:03 +0200

    Cleaned up some files

--- AudienceResponseSystem/src/main/java/com.ars.alpha/AudienceResponseSystemApplication.java
+++ AudienceResponseSystem/src/main/java/com.ars.alpha/AudienceResponseSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AudienceResponseSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AudienceResponseSystemApplication.class, args);
 	}
 
