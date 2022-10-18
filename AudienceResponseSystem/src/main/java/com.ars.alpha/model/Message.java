831e8af0d4952a4beeb0d5bef674f78f33e7ecfd
leafcathead <leafcathead@gmail.com>
2022-10-18 20:26:01 +0200

    Updated Dependencies
    There's a problem with the Spring Boot dependency we need to hook up to the server. I will troubleshoot later.

--- AudienceResponseSystem/src/main/java/com.ars.alpha/model/Message.java
+++ AudienceResponseSystem/src/main/java/com.ars.alpha/model/Message.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 // These will work once we know what database we use. For now they will cause errors.
 // @Entity
-// @Table(name = "Message")
+//@Table(name = "Message")
 public class Message {
 
     private UUID message;
