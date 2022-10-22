0889eb71a3d40ef534013d47f8210f1f54ac7768
leafcathead <leafcathead@gmail.com>
2022-10-22 16:35:34 +0200

    Minor Fixes
    -Typo fix
    -Fixed crash
    -Commented out something might also cause a crash.

--- AudienceResponseSystem/src/main/java/com.ars.alpha/AudienceResponseSystemApplication.java
+++ AudienceResponseSystem/src/main/java/com.ars.alpha/AudienceResponseSystemApplication.java
@@ -2,8 +2,9 @@ package com.ars.alpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AudienceResponseSystemApplication {
 
 	public static void main(String[] args) {
