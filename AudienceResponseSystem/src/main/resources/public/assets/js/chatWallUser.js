c82e23c7f9b51fc10c65cbce308850c304ddc1ad
jamous01 <115616951+jamous01@users.noreply.github.com>
2022-11-07 22:42:53 +0100

    chatWall user + owner JS

---
+++ AudienceResponseSystem/src/main/resources/public/assets/js/chatWallUser.js
@@ -0,0 +1,8 @@
+//store data on the localstorage will be removed by user manually
+localStorage.setItem('passw', '786')
+localStorage.setItem('password', '8D1F')
+
+
+//store data on the session storage will be removed by closing the browser
+sessionStorage.setItem('name', 'Kyle')
+sessionStorage.setItem('lastName', 'Smith')
